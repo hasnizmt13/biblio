c4a5a666d9746bd3a66352e0927deb00415dab51
ZOUMATA Hasni <hasnizoumata@gmail.com>
2024-05-08 16:06:27 +0200

    historique et réservation

--- src/component/Emprunt.jsx
+++ src/component/Emprunt.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import Commons from './Commons.jsx';
 
 function Emprunt() {
+  const reservs = [
+    { id: 1, name: 'Alice', age: 24, city: '22/05/2024'  },
+    { id: 2, name: 'Bob', age: 30, city: '22/05/2024'  },
+    { id: 3, name: 'Carla', age: 29, city: '22/05/2024'  }
+  ];
   return (
         <>
     <div className='pt-14 flex min-h-screen '>
@@ -10,8 +15,32 @@ function Emprunt() {
         <Commons/>
       </div>
 
-      <div className='bg-white w-full flex items-start mt-10 justify-center'>
-      
+      <div className='bg-white w-full flex-col items-start mt-20  justify-center'>
+          <h1 className="text-3xl text-darkBlue font-bold text-center md:text-left md:text-4xl ml-20 md:max-w-md">
+          Mes Livres empruntés
+          </h1>
+          <div className='mx-20'>
+            <table className="m-20 table-auto w-full mx-auto">
+              <thead className="bg-darkBlue">
+                <tr>
+                  <th className="px-4 py-2 text-white text-left">Nom du livre</th>
+                  <th className="px-4 py-2 text-white text-left">Date d'emprunt</th>
+                  <th className="px-4 py-2 text-white text-left">Date de retour prévue</th>
+                  <th className="px-4 py-2 text-white text-left">Date de retour réelle</th>
+                </tr>
+              </thead> 
+              <tbody>
+                {reservs.map(reserv => (
+                  <tr key={reserv.id} className="border-b">
+                    <td className="px-4 py-2">{reserv.name}</td>
+                    <td className="px-4 py-2">{reserv.age}</td>
+                    <td className="px-4 py-2">{reserv.city}</td>
+                    <td className="px-4 py-2">{reserv.city}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
       </div>
     </div>
 </>
--- src/component/Reservation.jsx
+++ src/component/Reservation.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 import Commons from './Commons.jsx';
 
 function Reservation() {
+
+  const reservs = [
+    { id: 1, name: 'Alice', age: '22/05/2024' , city: 'Paris' },
+    { id: 2, name: 'Bob', age: '22/05/2024' , city: 'Berlin' },
+    { id: 3, name: 'Carla', age:'22/05/2024' , city: 'New York' }
+  ];
+
   return (
     <>
     <div className='pt-14 flex min-h-screen '>
@@ -12,8 +19,30 @@ function Reservation() {
 
         </div>
 
-        <div className='bg-white w-full flex items-start mt-10 justify-center'>
-      
+        <div className='bg-white w-full flex-col items-start mt-20  justify-center'>
+          <h1 className="text-3xl text-darkBlue font-bold text-center md:text-left md:text-4xl ml-20 md:max-w-md">
+            Mes réservations
+          </h1>
+          <div className='mx-20'>
+            <table className="m-20 table-auto w-full mx-auto">
+              <thead className="bg-darkBlue">
+                <tr>
+                  <th className="px-4 py-2 text-white text-left">Nom du livre</th>
+                  <th className="px-4 py-2 text-white text-left">Date de réservation</th>
+                  <th className="px-4 py-2 text-white text-left">Statut</th>
+                </tr>
+              </thead> 
+              <tbody>
+                {reservs.map(reserv => (
+                  <tr key={reserv.id} className="border-b">
+                    <td className="px-4 py-2">{reserv.name}</td>
+                    <td className="px-4 py-2">{reserv.age}</td>
+                    <td className="px-4 py-2">{reserv.city}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
        
     </div>
